273f8e405923d5a73ec30e1cc086c267c54435a0
Himatkumar Purohit <himatpurohit1607@gmail.com>
2020-11-19 00:39:43 +0530

    Added JavaFX initialize method and UIThread references

--- Events/src/sample/Controller.java
+++ Events/src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -15,23 +17,63 @@ public class Controller {
     private Button byeButton;
     @FXML
     private Label outputLabel;
+    @FXML
+    private CheckBox clearCheckBox;
 
 //    public void sayHello(){
 //        System.out.println("Hello "+nameField.getText());
 //        outputLabel.setText("Hello "+nameField.getText());
 //    }
 
-    /**
-     *
-     * @param e is used to define Actionevent
-     */
-    public void sayHello(ActionEvent e){
+    public void initialize() {
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    public void sayHello(ActionEvent e) {
 //        System.out.println("Hello "+nameField.getText());
-        if (e.getSource().equals(helloButton)){
-            outputLabel.setText("Hello "+nameField.getText());
-        }else if (e.getSource().equals(byeButton)){
-            outputLabel.setText("Bye "+nameField.getText());
+        if (e.getSource().equals(helloButton)) {
+            outputLabel.setText("Hello " + nameField.getText().trim());
+        } else if (e.getSource().equals(byeButton)) {
+            outputLabel.setText("Bye " + nameField.getText().trim());
         }
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println(Platform.isFxApplicationThread()?"UIThread":"Background Thread");
+                    Thread.sleep(10000);
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println(Platform.isFxApplicationThread()?"UIThread":"Background Thread");
+                            outputLabel.setText("Runnable executed after 10sec");
+                        }
+                    });
+                } catch (InterruptedException e1) {
+
+                }
+            }
+        };
+
+        new Thread(task).start();
+
+        if (clearCheckBox.isSelected()) {
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
+        }
+    }
+
+    public void onKeyReleased() {
+        String text = nameField.getText();
+        boolean textEmpty = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(textEmpty);
+        byeButton.setDisable(textEmpty);
+    }
+
+    public void handleChange() {
+        System.out.println("CheckBox is " + (clearCheckBox.isSelected() ? "Selected" : "not Selected"));
     }
 
 }
